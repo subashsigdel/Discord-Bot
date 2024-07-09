37f5423aac6c36408914fcc93c088dd44e6f2db3
subashsigdel <info@subashsigdel.com.np>
2024-07-09 18:20:44 +0545

    token updated

--- .github/workflows/musicbot.py
+++ .github/workflows/musicbot.py
@@ -2,7 +2,11 @@ import discord
 from discord.ext import commands
 import yt_dlp
 import asyncio
-import os
+import os 
+
+# Read the secret token from the environment variable
+DISCORD_SECRET_TOKEN = os.getenv('DISCORD_SECRET_TOKE')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -21,7 +25,7 @@ class MusicBot(commands.Cog):
         voice_channel = ctx.author.voice.channel if ctx.author.voice else None
         if not voice_channel:
             return await ctx.send("voice channel ma ja paila ")
-
+        
         if not ctx.voice_client:
             await voice_channel.connect()
 
@@ -55,9 +59,7 @@ class MusicBot(commands.Cog):
 client = commands.Bot(command_prefix="geet ", intents=intents)
 
 async def main():
-    token = os.getenv('DISCORD_SECRET_TOKEN2')
-    print(f'Token: {token}')  # Add this line for debugging
     await client.add_cog(MusicBot(client))
-    await client.start(token)
+    await client.start(DISCORD_SECRET_TOKEN)
 
 asyncio.run(main())
